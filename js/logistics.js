70b15b6dcf2e6d0644e936d24c076205c46fc817
zhaojinshi <1163612848@qq.com>
2017-10-17 11:22:52 +0800

    '物流进度'

--- js/logistics.js
+++ js/logistics.js
@@ -29,7 +29,7 @@ $(function () {
            $('<div class="ullist"><ul><li><h2>物流状态：</h2><span>已签收</span></li><li><h2>承运公司：</h2><span>'+data.result.expTextName+'</span></li></ul><ul><li><h2>货运单号：</h2><span>'+data.result.mailNo+'</span></li><li><h2>官方电话：</h2><span>'+data.result.tel+'</span></li></ul></div>').appendTo($('.logistics-header'))
             for(let i=0;i<data.result.data.length;i++){
                 console.log(data.result.data)
-                $('<li><div></div><div class="liitem"><p>'+data.result.data[i].context+'</p><span>'+data.result.data[i].time+'</span></div></li>').appendTo($('section ul'))
+                $('<li><div class="processbox"><div class="processend"></div><div class="process"></div></div><div class="liitem"><p>'+data.result.data[i].context+'</p><span>'+data.result.data[i].time+'</span></div></li>').appendTo($('section ul'))
             }
         }
     })
